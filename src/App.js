b4835a9cafce50880f721bfee401ec68173c2938
himadri2110 <18it.himadri.shah@gmail.com>
2022-03-16 12:24:05 +0530

    feat: routes folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
-import { Products } from "./pages/Products/Products";
-import { Cart } from "./pages/Cart/Cart";
-import { Wishlist } from "./pages/Wishlist/Wishlist";
+import { NavRoutes } from "./Routes/NavRoutes";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-      </Routes>
+      <NavRoutes />
     </div>
   );
 }
