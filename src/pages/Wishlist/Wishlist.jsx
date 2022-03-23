da8b3733c17e73547289f70a0886eac2740608fc
himadri2110 <18it.himadri.shah@gmail.com>
2022-03-23 19:45:27 +0530

    feat: text for empty wishlist

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -3,6 +3,7 @@ import { Footer } from "../../components/Footer/Footer";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { useWishlist } from "../../contexts/wishlistContext";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
+import { Link } from "react-router-dom";
 
 const Wishlist = () => {
   const { wishlist } = useWishlist();
@@ -15,11 +16,15 @@ const Wishlist = () => {
         <section className="wishlist-wrapper">
           <div className="heading-3">My Wishlist ({wishlist.length})</div>
 
-          <section className="product-main">
-            {wishlist.map((wishlistItem) => (
-              <ProductCard product={wishlistItem} key={wishlistItem._id} />
-            ))}
-          </section>
+          {wishlist.length > 0 ? (
+            <section className="product-main">
+              {wishlist.map((wishlistItem) => (
+                <ProductCard product={wishlistItem} key={wishlistItem._id} />
+              ))}
+            </section>
+          ) : (
+            <p className="text-center">Your wishlist is empty!</p>
+          )}
         </section>
       </section>
 
